dce42460a189bbebe26b6ce2e07dc685bca48370
HIPHOP <hiphop@HIPHOPs-MacBook-Air.local>
2022-04-12 15:24:36 +0900

    find closest vaule in Binary Search Tree

---
+++ FindClosestValueBST.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class Program {
+  class BST {
+    var value: Int
+    var left: BST?
+    var right: BST?
+
+    init(value: Int) {
+      self.value = value
+      left = nil
+      right = nil
+    }
+  }
+
+  func findClosestValueInBST(tree: BST?, target: Int) -> Int {
+    // Write your code here.
+    return -1
+  }
+}
+
